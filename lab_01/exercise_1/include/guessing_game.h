a88862c45e6e80b1439aeedf7b5fe0e86bdf761d
joel-alvarado <joel.alvarado2@upr.edu>
2024-08-24 14:16:56 -0400

    Moved doxygen comments to header file, added include guards, removed parameter from GenerateRandomNumber(), added new driver method for Guessing Game

--- lab_01/exercise_1/include/guessing_game.h
+++ lab_01/exercise_1/include/guessing_game.h
@@ -1,5 +1,41 @@
 #include <stdbool.h>
 
-int GenerateRandomNumber(int N);
+#ifndef GUESSING_GAME_H
+#define GUESSING_GAME_H
+
+#define MAX_GUESS_NUMBER 10
+
+/**
+ * @brief Starts the guessing game. Will end when user guesses the number
+ * correctly.
+ *
+ * @param debug: If true, will print the system guess to the user.
+ *
+ */
+void GuessingGame(bool debug);
+
+/**
+ * @brief Generates a random number between 0 and MAX_GUESS_NUMBER
+ *
+ * @return int
+ */
+int GenerateRandomNumber();
+
+/**
+ * @brief Reads user guess from stdin.
+ *
+ * @return int
+ */
 int GetUserGuess();
+
+/**
+ * @brief Compares the user guess with the game's number.
+ *
+ * @param user_number
+ * @param correct_number
+ * @return true
+ * @return false
+ */
 bool VerifiyGuess(int user_number, int correct_number);
+
+#endif /* GUESSING_GAME_H */
