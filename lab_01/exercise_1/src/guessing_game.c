f9720a9b4cbec2144059b830c62ed4dc3c013be6
joel-alvarado <joel.alvarado2@upr.edu>
2024-08-24 14:17:43 -0400

    Moved main logic to GuessignGame(), implemented maxx number macro

--- lab_01/exercise_1/src/guessing_game.c
+++ lab_01/exercise_1/src/guessing_game.c
@@ -4,37 +4,38 @@
 #include <stdlib.h>
 #include <time.h>
 
-/**
- * @brief Returns a random number with the current time as seed.
- *
- * @return int
- */
-int GenerateRandomNumber(int N) {
+void GuessingGame(bool debug) {
+  printf("Guess a number between 1 and %d: \n", MAX_GUESS_NUMBER);
+  int correct_number = GenerateRandomNumber();
+
+  if (debug) {
+    printf("System chose: %d \n", correct_number);
+  }
+
+  while (true) {
+    printf("Your guess: ");
+    int user_number = GetUserGuess();
+    if (VerifiyGuess(user_number, correct_number)) {
+      printf("Correct! \n");
+      return;
+    } else {
+      printf("Wrong! \n");
+    }
+  }
+}
+
+int GenerateRandomNumber() {
   srand(time(NULL));  // Init seed to current time since UNIX 00:00:00 UTC
-  int generated_number = rand() % (N + 1);
+  int generated_number = (rand() % MAX_GUESS_NUMBER + 1);
   return generated_number;
 }
 
-/**
- * @brief Read user guess from stdin.
- *
- * @return int
- */
 int GetUserGuess() {
   int user_guess;
   scanf("%d", &user_guess);
   return user_guess;
 }
 
-/**
- * @brief Compares the user guess to the correct number and returns the
- * comparison.
- *
- * @param user_number
- * @param correct_number
- * @return true
- * @return false
- */
 bool VerifiyGuess(int user_number, int correct_number) {
   return user_number == correct_number;
 }
--- lab_01/exercise_1/src/main.c
+++ lab_01/exercise_1/src/main.c
@@ -1,20 +1,3 @@
-#include <stdio.h>
-
 #include "guessing_game.h"
 
-int main() {
-  while (true) {
-    int max_number = 9;
-    printf("Guess a number between 0 and %d: ", max_number);
-    int correct_number = GenerateRandomNumber(max_number);
-    int user_number = GetUserGuess();
-    printf("Correct %d \n", correct_number);
-
-    if (VerifiyGuess(user_number, correct_number)) {
-      printf("Correct! \n");
-      return 0;
-    } else {
-      printf("Wrong! \n");
-    }
-  }
-}
+int main() { GuessingGame(true); }
