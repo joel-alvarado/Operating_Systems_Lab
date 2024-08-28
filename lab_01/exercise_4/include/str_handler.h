ff30975d9e011ab9b6b1e9c34cb7756ef6df637e
joel-alvarado <joel.alvarado2@upr.edu>
2024-08-27 23:18:28 -0400

    added skeleton of str_handler library

---
+++ lab_01/exercise_4/include/str_handler.h
@@ -0,0 +1,26 @@
+#ifndef STR_HANDLER_H
+#define STR_HANDLER_H
+
+/**
+ * @brief Searches and replaces all occurences of find in src with replacement
+ * in dst.
+ *
+ * @param src: source string
+ * @param dst: replaced string
+ * @param find: char to find
+ * @param replacement: char to replace with
+ */
+void Replace(char *src, char *dst, char *find, char *replacement);
+
+/**
+ * @brief Searches and inserts ins into all occurences of find from src into
+ * dst.
+ *
+ * @param src
+ * @param dst
+ * @param find
+ * @param ins
+ */
+void Insert(char *src, char *dst, char find, char *ins);
+
+#endif /* STR_HANDLER_H */
---
+++ lab_01/exercise_4/src/str_handler.c
@@ -0,0 +1,5 @@
+#include "str_handler.h"
+
+void Replace(char *src, char *dst, char *find, char *replacement) {}
+
+void Insert(char *src, char *dst, char find, char *ins) {}
