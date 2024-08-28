c523f58d3452752793814448ffed7b4eeaa88755
joel-alvarado <joel.alvarado2@upr.edu>
2024-08-28 16:50:37 -0400

    remove old detail

--- lab_01/exercise_5/include/numlib.h
+++ lab_01/exercise_5/include/numlib.h
@@ -9,12 +9,6 @@
  * @param str
  *
  * @return char*
- *
- * @details This method is *supposed* to store the memory block of the new
- * string into str. However, in C, you cannot malloc() on an existing pointer.
- * This means you would need to malloc the necessary memory BEFORE calling this
- * method, which would conflict with the guidelines of this exercsie, which say
- * to allocate the memory in the method itself.
  */
 char *NumToStr(int num, char *str);
 
