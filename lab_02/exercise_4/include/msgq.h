590bea2924ef2c05de7372af32938bfaed50ba2b
joel-alvarado <joel.alvarado2@upr.edu>
2024-09-15 08:41:26 -0400

    Add small doxygen

--- lab_02/exercise_4/include/msgq.h
+++ lab_02/exercise_4/include/msgq.h
@@ -4,11 +4,19 @@
 #define MSG_ARRAY_SIZE 1000
 #define SUM_SIZE 1000000
 
+/**
+ * @brief Message struct to send using message queues
+ *
+ */
 struct message {
   long mtype;
   int marray[MSG_ARRAY_SIZE];
 };
 
+/**
+ * @brief Time struct to send timeval in message queues
+ *
+ */
 struct time {
   long mtype;
   struct timeval start;
