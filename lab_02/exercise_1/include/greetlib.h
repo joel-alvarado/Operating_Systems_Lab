64b0a538be81ad57202ba3d7547b29039f0aec87
joel-alvarado <joel.alvarado2@upr.edu>
2024-09-15 08:43:06 -0400

    add doxygen to printgreetings

--- lab_02/exercise_1/include/greetlib.h
+++ lab_02/exercise_1/include/greetlib.h
@@ -1,6 +1,10 @@
 #ifndef GREETLIB_H
 #define GREETLIB_H
 
+/**
+ * @brief Prints the current PID and sleeps for 10 seconds. Then says goodbye.
+ *
+ */
 void PrintGreetings();
 
 #endif /* GREETLIB_H */
