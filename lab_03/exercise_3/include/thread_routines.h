019418e74fe56c1d3f821a1ad30bcbd70861df37
joel-alvarado <joel.alvarado2@upr.edu>
2024-09-29 20:49:47 -0400

    added doxygen comments

--- lab_03/exercise_3/include/thread_routines.h
+++ lab_03/exercise_3/include/thread_routines.h
@@ -7,11 +7,54 @@
 
 #define FILE_NAME_LENGTH_MAX 128
 
+/**
+ * @brief Holds all shared data between threads
+ *
+ */
 typedef struct sThreadData tThreadData;
 
+/**
+ * @brief Thread routine that writes `tThreadData.input_buffer_size` random
+ * __uint8_t to `tThreadData.file_name`. Returns 0 if succesful and 1 if failed
+ * (eg. file already exists)
+ *
+ * @param data
+ * @return void*
+ */
 void *writeRandomIntegers(void *data);
+
+/**
+ * @brief Thread routine that reads `tThreadData.input_buffer_size` numbers into
+ * `tThreadData.input_buffer` from `tThreadData.file_name`. Signals
+ * `calculateSquareRoot` thread when new data is read into
+ * `tThreadData.input_buffer`
+ *
+ * @param data
+ * @return void*
+ */
 void *readIntoInputBuffer(void *data);
+
+/**
+ * @brief Thread routine that calculates the square root of
+ * `tThreadData.input_buffer` (whose size is same as
+ * `tThreadData.input_buffer_size`) and writes the result into
+ * `tThreadData.processed_buffer`. Signals `writeProcessedBuffer` when newly
+ * processed data is available. Also waits for `readIntoInputBuffer` for newly
+ * read data.
+ *
+ * @param data
+ * @return void*
+ */
 void *calculateSquareRoot(void *data);
+
+/**
+ * @brief Thread routine that reads from `tThreadData.processed_buffer` and
+ * writes the numbers to `tThreadData.output_file_name`. This routines waits to
+ * be signaled by `calculateSquareRoot` for newly processed numbers.
+ *
+ * @param data
+ * @return void*
+ */
 void *writeProcessedBuffer(void *data);
 void startThreads();
 
