693259a503010cb0227a7750e5688a9cbfe46632
joel-alvarado <joel.alvarado2@upr.edu>
2024-10-10 20:25:42 -0400

    doxygen added

--- project_2/include/supervisor.h
+++ project_2/include/supervisor.h
@@ -3,10 +3,41 @@
 
 #include <aio.h>
 
+/**
+ * @brief Starts supervisor process. Looks for a PID related to `imu_driver`. If
+ * not found, fork execute the imu_driver process.
+ *
+ */
 void StartIMUSupervisor();
+
+/**
+ * @brief Get the PID from a process name.
+ *
+ * @param process_name Process name to find.
+ * @return pid_t PID of `process_name`, -1 if not found.
+ */
 pid_t GetPidByName(const char *process_name);
+
+/**
+ * @brief Checks if imu_driver is running. If not, calls
+ * `StartNewIMUDriverProcess`.
+ *
+ * @param signum Signal number recieved (SIGUSR1 from timer).
+ */
 void CheckAndRestartIMUDriver(int signum);
+
+/**
+ * @brief Fork-executes a new `imu_driver` process.
+ *
+ * @return pid_t PID of the new `imu_driver` process.
+ */
 pid_t StartNewIMUDriverProcess();
+
+/**
+ * @brief Handles `SIGTERM` signal to exit gracefully.
+ *
+ * @param signum Signal recieved (SIGTERM)
+ */
 void HandleSIGTERM(int signum);
 
 #endif
